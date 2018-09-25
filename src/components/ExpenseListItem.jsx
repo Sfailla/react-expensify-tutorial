ad0b81327322f8c83d70a1e36c47c5c2d1116f4c
Your Name <sfailla1983@gmail>
2018-09-25 09:32:23 -0400

    added numeral library to format currency and formatted time in ExpenseItemList componenet

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => (
 	<div>
 		<h3>
 			<Link to={`/edit/${id}`}>{description}</Link>
 		</h3>
-		<p>Amount: {amount}</p>
-		<p>Created: {createdAt}</p>
+		<p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
+		<p>Created: {moment(createdAt).format('MMMM Do, YYYY')}</p>
 	</div>
 );
 
